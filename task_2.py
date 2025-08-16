d2deb482367fcafa7cba843381c8051e2b733825
Helsingl <e.a.grigorieva@yandex.ru>
2025-08-16 19:54:01 +0300

    task_2.py - вызовы текста

---
+++ task_2.py
@@ -0,0 +1,16 @@
+class Tester():
+
+    def __init__(self, name, deadline):
+        self.name = name
+        self.deadline = deadline
+
+    def work_hard(self):
+        if self.deadline ==True:
+            print(self.name, 'Что ж, ещё часок поработаю!')
+        else:
+            print(self.name, 'Можно отдыхать')
+
+tester_1 = Tester(name = 'tester_1', deadline = False)
+tester_1.work_hard()  # 'tester_1 Можно отдыхать'
+tester_2 = Tester(name = 'tester_2', deadline = True)
+tester_2.work_hard()   # 'tester_2 Что ж, ещё часок поработаю!' 
