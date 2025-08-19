5c936526a1970f5b0a31ba9dbe0e8b3654d78cc7
Helsingl <e.a.grigorieva@yandex.ru>
2025-08-19 15:16:44 +0300

    task_1.py/хотфикс

--- task_1.py
+++ task_1.py
@@ -4,18 +4,14 @@ total_min = 0
 for i in timelist:
     timelist_total = i.split()
     for j in timelist_total:
-        if 'h' in j: #часы переводим в минуты
+        if 'h' in j: # часы переводим в минуты
             timelist1 = j.replace('h', '')
             total_min += 60*(int(timelist1))
-        #timelist_n += t1
-        elif 'm' in j: #складываем минуты
+        elif 'm' in j: # складываем минуты
             timelist2 = j.replace('m', '')
             total_min += 1*(int(timelist2))
-        elif 's' in j:  #секунды переводим в минуты
+        elif 's' in j:  # секунды переводим в минуты
             timelist3 = j.replace('s', '') 
             total_min +=  (int(timelist3))/60
 
-
-print(int(total_min)) #общая сумма в минутах
-
-
+print(int(total_min)) # общая сумма в минутах
